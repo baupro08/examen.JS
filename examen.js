e559f042b5e35c4efd24d1e229146b08b7fbbeb7
baupro08 <137501523+baupro08@users.noreply.github.com>
2023-09-15 09:47:04 -0300

    Create examen.js

---
+++ examen.js
@@ -0,0 +1,27 @@
+/*
+function salarioAnual(SalarioA, SalarioM){
+
+    let salario = SalarioA / SalarioM
+    return salario
+}
+ console.log(calcularPromedio(21600, 12))
+ */
+
+
+/*
+ function SalarioMensual(SalarioM){
+
+let salario = SalarioM * 12
+return salario
+ }
+ console.log(SalarioMensual(1500))
+*/
+
+
+function calcularPromedio(nota1,nota2,nota3){
+
+
+    let promedio =(nota1 + nota2 + nota3) / 3
+    return promedio
+}
+alert(calcularPromedio(5,5,5))
